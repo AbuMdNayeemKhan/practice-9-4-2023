f83b555514f8e3d0dd6ef6644a1df3c8ce92885e
Abu Md Nayeem Khan <abumdnayeemkhan@gmail.com>
2023-04-09 16:56:49 +0600

    learning javascript

---
+++ tryCatchThrowFinally.js
@@ -0,0 +1,9 @@
+try{
+    console.log("Nayeem Khan");
+    console.log(num);
+}catch(errorData){
+    // console.log(errorData.message);
+    console.log("How are you?");
+}finally{
+    console.log("Front End Developer");  
+}
